e37621e181a58cb4da0d7c27d6d56a8e4def5a9c
Derek Babb <dbabb1@gmail.com>
2022-03-23 10:11:28 -0500

    Added code for authentication

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+def on_received_string(receivedString):
+    radio.send_number(radio_band)
+radio.on_received_string(on_received_string)
+
+def on_received_value(name, value):
+    if name == "password" and value == passcode:
+        pass
+    else:
+        pass
+radio.on_received_value(on_received_value)
+
+radio_band = 0
+passcode = 5
+radio_band = 4
+radio.set_group(radio_band)
+basic.show_icon(IconNames.HAPPY)
+
 def on_forever():
-    pass
+    radio.send_value("passcode", passcode)
+    basic.pause(2000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
 radio.onReceivedString(function (receivedString) {
     radio.sendNumber(radio_band)
 })
+radio.onReceivedValue(function (name, value) {
+    if (name == "password" && value == passcode) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
 let radio_band = 0
+let passcode = 0
+passcode = 5
 radio_band = 4
 radio.setGroup(radio_band)
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-	
+    radio.sendValue("password", passcode)
+    basic.pause(2000)
 })
